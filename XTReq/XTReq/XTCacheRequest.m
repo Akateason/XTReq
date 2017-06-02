4f36fcae67fc9873c6ebe87bc6649b3f8c8c160b
tianchen.xie <tianchen.xie@phicomm.com>
2017-06-02 16:01:25 +0800

    update nsdate+xttick

--- XTReq/XTReq/XTCacheRequest.m
+++ XTReq/XTReq/XTCacheRequest.m
@@ -246,7 +246,7 @@
                      else
                      {
                          resModel.response = [json yy_modelToJSONString] ;
-                         resModel.updateTime = [NSDate xt_getTickFromNow] ;
+                         resModel.updateTime = [NSDate xt_getNowTick] ;
                          [resModel xt_update] ; // db update
                      }
                  }
@@ -276,7 +276,7 @@
                       else
                       {
                           resModel.response = [json yy_modelToJSONString] ;
-                          resModel.updateTime = [NSDate xt_getTickFromNow] ;
+                          resModel.updateTime = [NSDate xt_getNowTick] ;
                           [resModel xt_update] ; // db update
                       }
                   }
